3a4f7039a8918fbd8798500051f2998e7ab6cd7a
yinyunsan <34375781+yinyunsan@users.noreply.github.com>
2018-10-27 12:33:48 +0800

    Update js1.js

--- js/js1.js
+++ js/js1.js
@@ -1,7 +1,7 @@
 function evil() {
     alert('hi,you press it')
     alert('evils are coming')
-    var n = "<script>i=1;while(i<=10){alert('hi');i++;}</script>";
+    var n = "<script>i=1;while(i<=1000){alert('hi,i'm here');i++;}</script>";
     var r = window.open("", "", "");
     r.opener = null,
     r.document.write(n),
