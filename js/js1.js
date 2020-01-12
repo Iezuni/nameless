234b7e2a84a7ed027a26b1e83db98a2204b5414a
yinyunsan <yinyunsan@foxmail.com>
2020-01-12 21:07:40 +0800

    //frame

--- js/js1.js
+++
@@ -1,8 +0,0 @@
-function evil() {
-    alert('hi,you press it')
-    alert('evils are coming')
-    var n = "<script>i=1;while(i<=1000){alert('hi,i am here');i++;}<\/script>";
-    var r = window.open("", "", "width=200,height=100");
-	r.document.write(n);
-	r.focus();
-}
